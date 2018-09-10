4e13a539fd349c157a762884f9671178314ca24b
Dan Rapp <drapp@proofpoint.com>
2018-09-10 15:20:11 -0600

    Fix script rename error

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup_options = dict(
     author='Resero-Labs',
     url='https://github.com/resero-labs/docker-utils.git',
     scripts=[
-        'scripts/aws-create-dock',
+        'scripts/create-dock',
         'scripts/build-image',
         'scripts/dock',
         'scripts/dock-sync',
