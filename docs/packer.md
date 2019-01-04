6f5ffe1ea39f4d0ff9465c9738f08bfd3c1ec89b
Dan Rapp <drapp@proofpoint.com>
2019-01-03 22:19:23 -0700

    Establish conventions for promoting new AMI to "production" Update nvidia drivers to 410.79 Single quote cmd on run-image command line

--- docs/packer.md
+++ docs/packer.md
@@ -1,6 +1,12 @@
 # To build new packer image
 
-```bash
-$ cd packer
-$ packer build resero-labs-nvidia-docker.packer
-```
+1. Update version in ami-name in `packer/resero-labs-nvidia-docker.packer`
+2. Update nvidia driver versions in `packer/setup-v1.sh`
+3. Run the following: 
+    ```bash
+    $ cd packer
+    $ packer build resero-labs-nvidia-docker.packer
+    ```
+4. after testing, use the AWS console to change the image currently named "resero-labs-nvidia-docker" to
+"resero-labs-nvidia-docker-<dated.version>", and change the name of the image just created, 
+"resero-labs-nvidia-docker-latest" to "resero-labs-nvidia-docker" 
--- packer/setup-v1.sh
+++ packer/setup-v1.sh
@@ -7,24 +7,26 @@ sleep 30
 sudo apt-get update
 sudo apt-get install -y \
     apt-transport-https \
-    aws-cli \
     ca-certificates \
     curl \
     gcc \
     make \
+    python3 \
+    python3-pip \
     software-properties-common
+pip3 install awscli --upgrade --user
 
 # get the latest nvidia drivers and install them
-wget -P /tmp http://us.download.nvidia.com/tesla/396.44/NVIDIA-Linux-x86_64-396.44.run
-chmod +x /tmp/NVIDIA-Linux-x86_64-396.44.run
-sudo /tmp/NVIDIA-Linux-x86_64-396.44.run -silent
+wget -qP /tmp http://us.download.nvidia.com/tesla/410.79/NVIDIA-Linux-x86_64-410.79.run
+chmod +x /tmp/NVIDIA-Linux-x86_64-410.79.run
+sudo /tmp/NVIDIA-Linux-x86_64-410.79.run -silent
 
 # now get docker and nvidia-docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
-sudo apt-get install -y docker-ce=18.06.0~ce~3-0~ubuntu
-sudo apt-get install -y nvidia-docker2
+sudo apt-get install -y docker-ce \
+    nvidia-docker2
