11970df1758e4d25499be5b072861e9bde26b095
Dan Rapp <drapp@proofpoint.com>
2019-03-31 09:52:43 -0600

    Handle different versions of docker starting with different -H options

--- packer/configure-docker.sh
+++ packer/configure-docker.sh
@@ -6,6 +6,20 @@
 # all key/cert setup is in register-dock script
 sudo usermod -aG docker ubuntu
 sudo systemctl stop docker
-sudo sed -i 's"dockerd\ -H\ unix://"dockerd"g' /lib/systemd/system/docker.service
+
+# remove the -H option from the docker service configuration
+#
+# some versions of docker have the line:
+# ExecStart=/usr/bin/dockerd -H fd://
+# others have the line:
+# ExecStart=/usr/bin/dockerd -H unix://
+#
+if grep -q "/usr/bin/dockerd -H fd://" /lib/systemd/system/docker.service; then
+    sudo sed -i 's"dockerd\ -H\ fd://"dockerd"g' /lib/systemd/system/docker.service
+fi
+if grep -q "/usr/bin/dockerd -H unix://" /lib/systemd/system/docker.service; then
+    sudo sed -i 's"dockerd\ -H\ unix://"dockerd"g' /lib/systemd/system/docker.service
+fi
+
 sudo systemctl daemon-reload
 sudo systemctl start docker
